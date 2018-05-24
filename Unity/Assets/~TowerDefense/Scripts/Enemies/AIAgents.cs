c30009bd8cb5b2e032f9305b4bc959f0ea5ca41d
zachary.dumlao1 <zachary.dumlao1@600900B211DE406>
2018-05-24 15:42:21 +1000

    Added enemies, enemy scripts and other stuff!

---
+++ Unity/Assets/~TowerDefense/Scripts/Enemies/AIAgents.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AI;
+
+namespace TowerDefense
+{
+    public class AIAgents : MonoBehaviour
+    {
+        public Transform target;
+        private NavMeshAgent nav;
+
+        // Use this for initialization
+        void Awake()
+        {
+            //Get reference to nav mesh
+            nav = GetComponent<NavMeshAgent>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //If there is a target
+            if(target)
+            {
+                nav.SetDestination(target.position);
+            }
+        }
+    }
+}
---
+++ Unity/Assets/~TowerDefense/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense
+{
+    public class Enemy : MonoBehaviour
+    {
+        public float health = 100f;
+
+        public void DealDamage(float damage)
+        {
+            //Deal damage to enemy
+            health -= damage;
+            //If there is no health
+            if(health <= 0)
+            {
+                //Kill off the gameObject
+                Destroy(gameObject);
+            }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
